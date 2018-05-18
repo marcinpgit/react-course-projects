13b321f075b91aa9b772637b2a799941e257c862
Marcin Płużyński <marcinp2012@gmail.com>
2018-05-18 13:55:33 +0200

    controlled inputs for filters

--- expensify-app/src/components/ExpenseDashboardPage.js
+++ expensify-app/src/components/ExpenseDashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import ExpenseList from './ExpenseList';
+import ExpanseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboardPage = () => (
     <div>
+        <ExpanseListFilters />
         <ExpenseList />
     </div>
 );
